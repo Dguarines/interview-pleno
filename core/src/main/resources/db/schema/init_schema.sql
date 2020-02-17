f2df99fd501530026e404b0e5ccf30a20ae547df
Deivison <dguarines@gmail.com>
2020-02-17 10:28:52 -0300

    enabling docker

--- core/src/main/resources/db/schema/init_schema.sql
+++ core/src/main/resources/db/schema/init_schema.sql
@@ -10,6 +10,6 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA interview_service TO brainweb;
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA interview_service TO brainweb;
 
 SET SCHEMA 'interview_service';
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA 'interview_service'; 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 GRANT EXECUTE ON FUNCTION uuid_generate_v4() TO brainweb;
 
